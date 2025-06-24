9ef8acd42e0ef9434542c1da41254aab8e93565c
Vincent Jin <vincent@stytch.com>
2025-06-24 11:51:13 -0700

    oauth protected resource support

--- api/lib/auth.ts
+++ api/lib/auth.ts
@@ -34,9 +34,15 @@ export const stytchBearerTokenAuthMiddleware = createMiddleware<{
     Bindings: Env,
 }>(async (c, next) => {
     const authHeader = c.req.header('Authorization')
+    const url = new URL(c.req.url);
 
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        throw new HTTPException(401, {message: 'Missing or invalid access token'})
+        const wwwAuthValue = `Bearer error="Unauthorized", error_description="Unauthorized", resource_metadata="https://${url.origin}/.well-known/oauth-protected-resource"`;
+        const responseHeaders = new Headers();
+        
+        responseHeaders.set('WWW-Authenticate', wwwAuthValue);
+        const res = new Response(null, {status: 401, headers: responseHeaders})
+        throw new HTTPException(401, {message: 'Missing or invalid access token', res: res})
     }
     const accessToken = authHeader.substring(7);
 
