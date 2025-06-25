86fc32082b04bee29a5f9875e05affca0aecf1d7
Vincent Jin <vincent@stytch.com>
2025-06-25 12:49:53 -0700

    use singular issuer

--- api/lib/auth.ts
+++ api/lib/auth.ts
@@ -68,14 +68,9 @@ async function validateStytchJWT(token: string, env: Env) {
         jwks = createRemoteJWKSet(new URL(getStytchOAuthEndpointUrl(env, '.well-known/jwks.json')))
     }
 
-    let expectedIssuer = `stytch.com/${env.STYTCH_PROJECT_ID}`
-    if (env.STYTCH_DOMAIN) {
-        expectedIssuer = `https://${env.STYTCH_DOMAIN}`
-    }
-
     return await jwtVerify(token, jwks, {
         audience: env.STYTCH_PROJECT_ID,
-        issuer: [expectedIssuer],
+        issuer: [`https://${env.STYTCH_DOMAIN}`],
         typ: "JWT",
         algorithms: ['RS256'],
     })
